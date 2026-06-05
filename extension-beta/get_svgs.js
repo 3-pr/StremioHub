99ba863a0b4186495b851393cf1033ec7ec8fc99
Yasser <7rb@dr.com>
2026-06-05 05:14:48 +0300

    chore: remove beta folder and download_avatars script

--- extension-beta/get_svgs.js
+++
@@ -1,6 +0,0 @@
-const https = require('https');
-
-async function search(icon) {
-    console.log("Searching for " + icon);
-}
-// Actually, it's easier to just use simple icons or search github for SVG packs.
